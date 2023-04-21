2ea2a1882e62b8ad5e4fc8bd51930683fa32d873
Adrijagit <125468532+Adrijagit@users.noreply.github.com>
2023-04-21 20:23:14 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -17,7 +17,7 @@ def main():
 if __name__ == '__main__':
     main()\
 #@st.cache(persist=True)
-data= pd.read_csv('C:/Users/KALYAN KUMAR GUHA/Desktop/ml-deploy/mushrooms.csv')
+data= pd.read_csv('mushrooms.csv')
 label= LabelEncoder()
 for i in data.columns:
     data[i] = label.fit_transform(data[i])
